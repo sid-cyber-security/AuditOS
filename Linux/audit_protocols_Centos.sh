39e334ab1f325bc74a3f32aa613a7c1891371528
Sergey <130347545+sid-cyber-security@users.noreply.github.com>
2023-05-12 17:14:21 +0500

    Update audit_protocols_Centos.sh

--- Linux/audit_protocols_Centos.sh
+++ Linux/audit_protocols_Centos.sh
@@ -2,7 +2,7 @@
 
 # Проверка наличия сервисов
 services=(
-    "telnet:telnetd:23:Telnet не зашифрованный протокол передачи данных. Рекомендуется использовать SSH."
+    "telnet:telnet-server:23:Telnet не зашифрованный протокол передачи данных. Рекомендуется использовать SSH."
     "ftp:vsftpd:21:FTP - протокол передачи файлов без шифрования. Рекомендуется использовать SFTP/FTPS или SCP."
     "rsh:rsh-server:514:RSH - старый протокол удаленного выполнения команд. Рекомендуется использовать SSH."
     "http:httpd:80:HTTP - не безопасный протокол передачи данных для веб-серверов. Рекомендуется использовать HTTPS с SSL/TLS-шифрованием."
@@ -12,13 +12,15 @@ services=(
 # Проверяем установленные сервисы
 for service_str in "${services[@]}"; do
     IFS=":" read -ra service <<< "$service_str"
-    if rpm -qa | grep -q "${service[1]}" || netstat -pnltu | grep -qw ":${service[2]}"; then
-        echo "${service[0]} (порт ${service[2]}) найден: ${service[3]}"
+    if rpm -qa | grep -q "${service[1]}"; then
+        if netstat -pnltu | grep -qw ":${service[2]}"; then
+            echo "${service[0]} (порт ${service[2]}) найден: ${service[3]}"
+        fi
     fi
 done
 
 # Проверяем наличие не безопасных протоколов
-if grep -q -E "rlogin|rsh|rexec" /etc/securetty; then
+if [ -f "/etc/securetty" ] && grep -q -E "rlogin|rsh|rexec" /etc/securetty; then
     echo "Rlogin, Rexec и Rsh (порты 512-514) найдены: старые протоколы с низким уровнем безопасности. Рекомендуется использовать SSH."
 fi
 
@@ -26,7 +28,7 @@ fi
 nis_services=("ypserv" "ypbind" "rpc.yppasswdd" "rpc.ypupdated" "rpc.ypxfrd")
 for nis_service in "${nis_services[@]}"; do
     if rpm -qa | grep -q "$nis_service"; then
-        echo "Сервис $nis_service (NIS или NIS+) найден: старые и небезопасные системы. Рекомендуется перейти на более современные системы."
+        echo "Сервис $nis_service(NIS или NIS+) найден: старые и небезопасные системы. Рекомендуется перейти на более современные системы."
     fi
 done
 
@@ -42,13 +44,15 @@ fi
 # Проверяет версии SSL
 ssl_versions=("SSLv2" "SSLv3")
 for version in "${ssl_versions[@]}"; do
-    if grep -qw "$version" /etc/httpd/conf/* || grep -qw "$version" /etc/nginx/*; then
+    if [ -d "/etc/httpd" ] && find /etc/httpd -type f | xargs grep -qw "$version"; then
+        echo "${version} найден: устаревшая версия TLS. Рекомендуется отключить и использовать TLS 1.2 или выше."
+    elif [ -d "/etc/nginx" ] && find /etc/nginx -type f | xargs grep -qw "$version"; then
         echo "${version} найден: устаревшая версия TLS. Рекомендуется отключить и использовать TLS 1.2 или выше."
     fi
 done
 
 # Проверяем версии SNMP
-if rpm -qa | grep -q "net-snmp" || rpm -qa | grep -q "snmp"; then
+if rpm -qa | grep -q "net-snmp"; then
     if ! grep -qw "v3" /etc/snmp/snmpd.conf; then
         echo "SNMPv1 и SNMPv2 найдены: первые версии протокола SNMP. Рекомендуется использовать SNMPv3 для аутентификации и шифрования данных."
     fi
